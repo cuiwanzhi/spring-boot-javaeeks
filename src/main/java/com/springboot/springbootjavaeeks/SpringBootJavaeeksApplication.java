8f3855efbd5f1dd9f1b916ff44d6fd6bd5f9210e
cuiwanzhi <2368314078@qq.com>
2020-07-03 16:39:51 +0800

    第一次创建好项目

---
+++ src/main/java/com/springboot/springbootjavaeeks/SpringBootJavaeeksApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.springbootjavaeeks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJavaeeksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJavaeeksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/springbootjavaeeks/SpringBootJavaeeksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springbootjavaeeks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJavaeeksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
