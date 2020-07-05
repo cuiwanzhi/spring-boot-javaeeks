a802cff2c04bc97d3c7f0323e56e13cb35685f0e
cuiwanzhi <2368314078@qq.com>
2020-07-06 01:07:24 +0800

    完成了管理员对用户的的增删改查 接下来做页面的美化

--- src/main/java/com/springboot/springbootjavaeeks/controller/ControController.java
+++ src/main/java/com/springboot/springbootjavaeeks/controller/ControController.java
@@ -1,5 +1,7 @@
 package com.springboot.springbootjavaeeks.controller;
 
+import com.springboot.springbootjavaeeks.bean.MyDateConverter;
+import com.springboot.springbootjavaeeks.bean.User;
 import com.springboot.springbootjavaeeks.mapper.ControllerMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,6 +9,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author cuiwanzhi
@@ -16,6 +20,9 @@ import javax.servlet.http.HttpServletRequest;
 public class ControController {
     @Autowired
     ControllerMapper controllerMapper;
+
+    @Autowired
+    MyDateConverter myDateConverter;
 
     @RequestMapping("/controllerLogin")
     public String controllerLogin(com.springboot.springbootjavaeeks.bean.Controller logincontroller, Model model, HttpServletRequest request) {
@@ -27,4 +34,25 @@ public class ControController {
         }
         return "controllerLogin";
     }
+
+    @RequestMapping("/getAllUser")
+    public String getAllUser(Model model,HttpServletRequest request) {
+        List<User> allUser = controllerMapper.getAllUser();
+        model.addAttribute("allUser", allUser);
+        return "selectUser";
+    }
+
+    @RequestMapping("/deleteUser")
+    public String deleteUser(User user) {
+        controllerMapper.deleteUser(user.getId());
+        return "redirect:/getAllUser";
+    }
+
+    @RequestMapping("/updateUserPassword")
+    public String selectUser(Model model,User user) {
+        controllerMapper.updateUserPassword(user.getId());
+        model.addAttribute("msg", "密码还原成功");
+        return "forward:/getAllUser";
+    }
+
 }
--- src/main/java/com/springboot/springbootjavaeeks/mapper/ControllerMapper.java
+++ src/main/java/com/springboot/springbootjavaeeks/mapper/ControllerMapper.java
@@ -1,8 +1,14 @@
 package com.springboot.springbootjavaeeks.mapper;
 
 import com.springboot.springbootjavaeeks.bean.Controller;
+import com.springboot.springbootjavaeeks.bean.User;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author cuiwanzhi
@@ -12,4 +18,16 @@ import org.apache.ibatis.annotations.Select;
 public interface ControllerMapper {
     @Select("select * from controller where id=#{id}")
     public Controller getController(int id);
+
+    @Update("update  User set password ='000000' where id=#{id}")
+    public int updateUserPassword(int id);
+
+    @Select("select * from user ")
+    public List<User> getAllUser();
+
+    @Delete("delete from user where id=#{id}")
+    public int deleteUser(int id);
+
+    @Update("update user set user_name=#{user_name} , password=#{password} ,name=#{name} , gender=#{gender},birthday=#{dirthday} ,contact=#{contact} , e_mail=#{e_mail} where id= #{id}")
+    public int updateUser(int id ,String user_name, String password , String name , String gender , Date birthday,String contact ,String e_mail);
 }
